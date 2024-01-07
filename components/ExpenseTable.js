d22f054cde569a36a5364f9a85e7d867eb108d75
Solanki Gaurav <solankigaurav2107@gmail.com>
2024-01-07 14:08:46 +0530

    list expense api integration

--- components/ExpenseTable.js
+++ components/ExpenseTable.js
@@ -1,8 +1,7 @@
 import Data from "/resources/data.json";
 import Constants from "/resources/constants.js";
 
-export default function ExpenseTable() {
-  const expenseData = Data.data;
+export default function ExpenseTable({ list }) {
   return (
     <section className="antialiased bg-gray-100 text-gray-600 h-screen px-4">
       <div className="flex flex-col justify-center h-full">
@@ -31,29 +30,17 @@ export default function ExpenseTable() {
                       <div className="font-semibold text-left">Note</div>
                     </th>
                     <th className="p-2 whitespace-nowrap">
-                      <div className="font-semibold text-left">Priority</div>
-                    </th>
-                    <th className="p-2 whitespace-nowrap">
                       <div className="font-semibold text-right">Amount</div>
                     </th>
                   </tr>
                 </thead>
                 <tbody className="text-sm divide-y divide-gray-100">
-                  {expenseData.map((data, key) => (
+                  {list.map((data, key) => (
                     <tr key={key}>
                       <td className="p-2 whitespace-nowrap">
                         <div className="flex items-center">
-                          {/* <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3"> */}
-                            {/* <img
-                              className="rounded-full"
-                              src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg"
-                              width="40"
-                              height="40"
-                              alt="Alex Shatov"
-                            /> */}
-                          {/* </div> */}
                           <div className="font-medium text-gray-800">
-                            {key+1}
+                            {key + 1}
                           </div>
                         </div>
                       </td>
@@ -62,17 +49,14 @@ export default function ExpenseTable() {
                       </td>
                       <td className="p-2 whitespace-nowrap">
                         <div className="text-left">
-                          {data.category}
+                          {data.category.name}
                         </div>
                       </td>
                       <td className="p-2 whitespace-nowrap">
-                        <div className={`text-lg text-left text-${data.type === 'dr' ? 'red': 'green'}-500 font-medium`}>{Constants[data.type]}</div>
+                        <div className={`text-lg text-left text-${data.type === 'dr' ? 'red' : 'green'}-500 font-medium`}>{Constants[data.type]}</div>
                       </td>
                       <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{data.note}</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">{data.priority}</div>
+                        <div className="text-left">{data.description}</div>
                       </td>
                       <td className="p-2 whitespace-nowrap">
                         <div className={`text-right text-${data.type === 'dr' ? 'red' : 'green'}-500 font-medium`}>{data.amount}</div>
--- pages/expense/index.js
+++ pages/expense/index.js
@@ -2,11 +2,17 @@ import ExpenseTable from "components/ExpenseTable";
 import BaseLayout from "components/layouts/BaseLayout";
 import Heading from "components/shared/Heading";
 
-export default function Expenses() {
+export default function Expenses({ list }) {
   return (
     <BaseLayout>
       <Heading heading={"Expense"} />
-      <ExpenseTable />
+      <ExpenseTable list={list} />
     </BaseLayout>
   );
 }
+
+export const getServerSideProps = (async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_BASE_URL}/expense`)
+  const data = await res.json();
+  return { props: { list: data } }
+});
