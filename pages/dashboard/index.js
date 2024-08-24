6b07c6461cf58780be876eb507a7afba8124cc7c
Solanki Gaurav <solankigaurav2107@gmail.com>
2024-08-24 23:50:14 +0530

    add liability card in dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -6,12 +6,18 @@ export default function Dashboard({ serverData }) {
     <BaseLayout>
       <div className="grid gap-4 sm:grid-cols-1 lg:grid-cols-4 lg:gap-8 md:grid-cols-2 p-8 pt-20">
         <StatCard title={"Total credits of"} value={serverData.total_credit_amount} />
+
         <StatCard title={"Total expenses of"} value={serverData.total_debit_amount}
           info={`${serverData.total_debit_percent}%`} staticText={`of total expenses accounted`} />
-        <StatCard title={"Total investments of"} value={serverData.total_invested_amount} 
-        info={`${serverData.total_invested_percent}%`} staticText={`allocated for investments`} />
-        <StatCard title={"Total savings of"} value={serverData.total_amount} 
-        info={`${serverData.total_savings_percent}%`} staticText={`set aside as savings`}/>
+
+        <StatCard title={"Total liability of"} value={serverData.total_liability_amount}
+          info={`${serverData.total_liability_percent}%`} staticText={`of total liability accounted`} />
+
+        <StatCard title={"Total investments of"} value={serverData.total_invested_amount}
+          info={`${serverData.total_invested_percent}%`} staticText={`allocated for investments`} />
+        <StatCard title={"Total savings of"} value={serverData.total_amount}
+          info={`${serverData.total_savings_percent}%`} staticText={`set aside as savings`} />
+
       </div>
     </BaseLayout>
   )
